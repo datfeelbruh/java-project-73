41e654e8e0e80add908ad64acf570f55ac7e6f08
datfeelbruh <datfeelman@gmail.com>
2023-03-28 06:09:37 +0300

    step 11

---
+++ src/main/java/hexlet/code/config/rollbar/RollbarConfig.java
@@ -0,0 +1,32 @@
+package hexlet.code.config.rollbar;
+
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({
+    "hexlet.code.app"
+})
+public class RollbarConfig {
+    @Value("${rollbar_token:}")
+    private String rollbarToken;
+    @Value("${spring.profiles.active:}")
+    private String activeProfile;
+
+    @Bean
+    public Rollbar rollbar() {
+        return new Rollbar(getRollbarConfig(rollbarToken));
+    }
+
+    private Config getRollbarConfig(String rollbarToken) {
+        return RollbarSpringConfigBuilder.withAccessToken(rollbarToken)
+                .environment("production")
+                .enabled(activeProfile.equals("prod"))
+                .build();
+    }
+}
